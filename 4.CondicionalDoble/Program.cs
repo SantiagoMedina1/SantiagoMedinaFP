adc5c34b3ae1f6d1710a8053622c8ca39da8dcf8
SantiagoMedina1 <santiago.medina@upb.edu.co>
2025-08-13 15:30:50 -0500

    Add files via upload

---
+++ 4.CondicionalDoble/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _4.CondicionalDoble
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre = "";
+            int sueldo = 0;
+
+            Console.WriteLine("Ingrese el nombre");
+            nombre=Console.ReadLine();
+
+            Console.WriteLine("Ingrese el sueldo");
+            sueldo = Int32.Parse(Console.ReadLine());
+
+            if (sueldo>=3000)
+            {
+                //Verdadera
+                Console.WriteLine($"La persona {nombre}, debe abonar impuestos");
+            }
+            else
+            {
+                //Falsa
+                Console.WriteLine($"La persona {nombre}, no abonar impuestos");
+
+            }
+
+
+        }
+    }
+}
