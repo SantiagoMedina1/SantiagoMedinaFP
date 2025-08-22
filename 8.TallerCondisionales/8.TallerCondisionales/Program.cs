08788fc272904cd78eabb1af529ba59e8e3ebba1
SantiagoMedina1 <santiago.medina@upb.edu.co>
2025-08-22 15:34:44 -0500

    Add files via upload

---
+++ 8.TallerCondisionales/8.TallerCondisionales/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _8.TallerCondisionales
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            int num3;
+            int mayor;
+            Console.WriteLine("Ingrese el núemro 1: ");
+            num1 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el núemro 2: ");
+            num2 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el núemro 3: ");
+            num3 = Int32.Parse(Console.ReadLine());
+
+            if (num1 == num2 || num1 == num3 || num2 == num3)
+            {
+                Console.WriteLine("Debe imgresar numeros diferentes");
+            }
+            else
+            {
+                if num1 > num2
+
+        }
+    }
+}
