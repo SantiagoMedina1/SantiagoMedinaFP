a5b4192336b6d687badfdb61b2009ac43f101ea5
SantiagoMedina1 <santiago.medina@upb.edu.co>
2025-09-03 15:32:31 -0500

    Add files via upload

---
+++ 13.While5/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _13.While5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Escribir un algoritmo que cuente cuantos números pares hay en un rango de números del 1 al número n.
+
+            int rango = 0;
+            int contador = 1;
+            int cantidadPares = 0;
+
+            Console.WriteLine("Ingrese el rango de números a evaluar");
+            rango = int.Parse(Console.ReadLine());
+
+            while (contador <= rango)
+            {
+                if (contador % 2 == 0)
+                {
+                    cantidadPares++;
+                }
+                contador++;
+            }
+            Console.WriteLine($"La cantidad de números pares del 1 hasta {rango}, es: {cantidadPares}");
+        }
+    }
+}
