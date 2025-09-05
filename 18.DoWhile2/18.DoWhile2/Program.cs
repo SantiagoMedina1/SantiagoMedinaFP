59ceaf47011b75c53f2ba3bd21642fe8d8b18118
SantiagoMedina1 <santiago.medina@upb.edu.co>
2025-09-05 15:30:11 -0500

    Add files via upload

---
+++ 18.DoWhile2/18.DoWhile2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _18.DoWhile2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*18. Algortimo que le pida a un grupo de usuarios su nombre, número de cuenta y saldo; para cada usuario se debe hacer lo siguiente
+            
+            1. Si el saldo es mayor a 3.000.000, sacar un mensaje que diga el nombre del usuario, el número de cuenta, el saldo y "Es apto para crédito"
+
+            2. Si el saldo es menor a 3
+        }
+    }
+}
