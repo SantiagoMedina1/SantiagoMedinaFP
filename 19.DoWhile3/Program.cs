1d1d59bb60ac5699b8e060ef668278d4255ea43e
SantiagoMedina1 <santiago.medina@upb.edu.co>
2025-09-10 15:37:18 -0500

    Add files via upload

---
+++ 19.DoWhile3/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _19.DoWhile3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*19. En un grupo de 100 alumnos, se desea saber cuántos de los estudiantes son hombres y cuántos son mujeres, 
+            además se desea saber cuántos son mayores de edad y cuántos no.*/
+
+            int cantidadEstudiantes = 0;
+            int edad = 0;
+            int genero = 0;
+            int cantidadHombres = 0;
+            int cantidadMujeres = 0;
+
+            do
+            {
+                Console.WriteLine($"Estudiante {cantidadEstudiantes}:");
+                Console.WriteLine("Ingrese su edad:");
+                edad=int.Parse( Console.ReadLine() );
+                Console.WriteLine("Ingrese su genero");
+                Console.WriteLine("1.Hombre     2.Mujer");
+                genero = int .Parse( Console.ReadLine() );
+
+                if (genero)
+                {
+
+                }
+
+            } 
+        }
+    }
+}
