bda40aa37fbf6f93621678df7fac4af3dc48dd18
SantiagoMedina1 <santiago.medina@upb.edu.co>
2025-09-17 15:44:49 -0500

    Update Program.cs

--- 19.DoWhile3/Program.cs
+++ 19.DoWhile3/Program.cs
@@ -4,30 +4,43 @@
     {
         static void Main(string[] args)
         {
-            /*19. En un grupo de 100 alumnos, se desea saber cuántos de los estudiantes son hombres y cuántos son mujeres, 
-            además se desea saber cuántos son mayores de edad y cuántos no.*/
-
-            int cantidadEstudiantes = 0;
+            int CantidadEstudiantes = 0;
             int edad = 0;
             int genero = 0;
-            int cantidadHombres = 0;
-            int cantidadMujeres = 0;
+            int CantidadHombres = 0;
+            int CantidadMujeres = 0;
+            int CantidadMayores = 0;
+            int CantidadMenores = 0;
+            int cantidad = 200;
 
             do
             {
-                Console.WriteLine($"Estudiante {cantidadEstudiantes}:");
-                Console.WriteLine("Ingrese su edad:");
-                edad=int.Parse( Console.ReadLine() );
-                Console.WriteLine("Ingrese su genero");
-                Console.WriteLine("1.Hombre     2.Mujer");
-                genero = int .Parse( Console.ReadLine() );
+                Console.WriteLine($"Estudiante {CantidadEstudiantes}: ");
+                Console.WriteLine("Ingrese su edad");
+                edad = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese su género");
+                Console.WriteLine("1.Hombre   2.Mujer");
+                genero = int.Parse(Console.ReadLine());
 
-                if (genero)
+                if (genero == 1)
                 {
-
+                    CantidadHombres++;
                 }
+                else
+                {
+                    CantidadMujeres++;
+                }
+                if (edad >= 18)
+                {
+                    CantidadMayores++;
+                }
+                else
+                {
+                    CantidadMayores++;
+                }
+            } while (CantidadEstudiantes < 200);
 
-            } 
+            Console.WriteLine($"De los 100 estudiantes, {CantidadHombres} son hombres, {CantidadMujeres} son mujeres, {CantidadMayores} son son mayores de edad, {CantidadMenores} son menores de edad");
         }
     }
 }
