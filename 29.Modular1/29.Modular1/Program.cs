9c5822a331d802aacc8052bc77fd6e3357451e89
SantiagoMedina1 <santiago.medina@upb.edu.co>
2025-10-03 15:27:30 -0500

    Add files via upload

---
+++ 29.Modular1/29.Modular1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _29.Modular1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] multiplosDe5 = new int[CapturarFilas(),CapturarColumnas()];
+        }
+
+        static int CapturarFilas()
+        {
+            Console.WriteLine("Ingrese el número de filas para la matriz");
+            int filas=int.Parse(Console.ReadLine());
+            return filas;
+        }
+
+        static int CapturarColumnas()
+        {
+            Console.WriteLine("Ingrese el número de columnas para la matriz");
+            int columnas = int.Parse(Console.ReadLine());
+            return columnas;
+        }
+
+        static int[,] LlenarMatriz(int[,]matriz)
+        {
+            return matriz;
+        }
+
+
+    }
+}
